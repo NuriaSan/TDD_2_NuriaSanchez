f8d3bccf4ca43a288091c0798dfb6dc7fb227baa
Nuria Sanchez <nuriasanchezjordan@gmail.com>
2023-04-01 14:25:28 +0200

    Primer Commit y test ok

---
+++ src/Coche.java
@@ -0,0 +1,6 @@
+
+public class Coche {
+
+	public Int velocidad;
+	
+}
---
+++ src/TestsCoche.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TestsCoche {
+
+	@Test
+	public void test_al_crear_un_coche_su_velocidad_es_cero(){
+        Coche nuevoCoche = new Coche();
+        Assertions.assertEquals (0, nuevoCoche.velocidad);
+     
+	}
+
+}
